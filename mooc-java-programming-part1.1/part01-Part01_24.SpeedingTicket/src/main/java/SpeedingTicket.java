3957bdb88143dd18806d24e1eb82dce8cf3dbfca
Md. Surat-E- Mostafa <surat153@gmail.com>
2020-04-21 00:33:26 +0300

    Add files via upload

---
+++ mooc-java-programming-part1.1/part01-Part01_24.SpeedingTicket/src/main/java/SpeedingTicket.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class SpeedingTicket {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Write your program here. 
+        System.out.println("Give speed:");
+        int data = Integer.valueOf(scanner.nextLine());
+        //int data = 135;
+        
+        if (data > 120) {
+            System.out.println("Speeding ticket!");
+        }
+        
+    }
+}
---
+++ mooc-java-programming-part1.1/part01-Part01_26.Orwell/src/main/java/Orwell.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Orwell {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Give a number:");
+        int data = Integer.valueOf(scan.nextLine());
+        if (data == 1984) {
+            System.out.println("Orwell");
+        }
+        
+
+        // Write your program here
+    }
+}
